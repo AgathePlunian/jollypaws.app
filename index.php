e8a2c638219275ec54accd97f6b8ead6157801af
Bastien <bastien.labouche96@orange.fr>
2021-09-06 17:48:42 +0200

    Retrait erreurs

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-	error_reporting(-1);
-	ini_set('display_errors', 'On');
+	// error_reporting(-1);
+	// ini_set('display_errors', 'On');
 
 	function get_lang($lang_list){
 		if(isset($_GET['lang']) && strlen($_GET['lang']) > 0 && in_array($_GET['lang'], $lang_list)){
