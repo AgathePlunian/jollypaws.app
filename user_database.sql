c656cde59ac26be0c7428265d88d301dc6d204aa
BastienL <bastien.labouche96@orange.fr>
2021-10-04 10:40:57 +0200

    modification structure bdd

--- user_database.sql
+++ user_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : jeu. 30 sep. 2021 à 15:56
+-- Généré le : lun. 04 oct. 2021 à 10:39
 -- Version du serveur :  10.3.31-MariaDB-0ubuntu0.20.04.1
 -- Version de PHP : 7.4.3
 
@@ -44,7 +44,30 @@ CREATE TABLE `articles` (
 --
 
 INSERT INTO `articles` (`id`, `author_id`, `title`, `content`, `main_image`, `creation_date`, `last_change_date`, `publish_date`) VALUES
-(1, 1, 'Mon premier article !', '[h2] Résumé [/h2]\r\n				\r\n[h2] A retenir [/h2]\r\n\r\n[h2] En savoir plus [/h2]\r\n										', 'media/f8bed9544d4d4713.png', '2021-09-30 13:54:26', '2021-09-30 13:54:41', NULL);
+(1, 1, 'Mon premier article !', '[h2] Résumé [/h2]\r\ntoto\r\n\r\n\r\n[h2] A retenir [/h2]\r\n\r\n[h2] En savoir plus [/h2]\r\n													', 'media/2d4c2ae7c068d579.png', '2021-09-30 13:54:26', '2021-09-30 14:35:33', NULL),
+(2, 1, 'Mon super article !', '[h2] Résumé [/h2]\r\nCeci est un article de présentation\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n[h2] A retenir [/h2]\r\nAvec Agathe on est beaucoup trop balèzes\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n[h2] En savoir plus [/h2]\r\nPas grand chose à rajouter			', 'media/b8f4aa4c27896921.png', '2021-09-30 14:34:42', '2021-09-30 14:39:20', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articles_categories`
+--
+
+CREATE TABLE `articles_categories` (
+  `id_article` int(255) NOT NULL,
+  `id_category` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -53,7 +76,7 @@ INSERT INTO `articles` (`id`, `author_id`, `title`, `content`, `main_image`, `cr
 --
 
 CREATE TABLE `permissions` (
-  `id` int(11) NOT NULL,
+  `id` int(255) NOT NULL,
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -71,7 +94,7 @@ INSERT INTO `permissions` (`id`, `name`) VALUES
 --
 
 CREATE TABLE `permissions_users` (
-  `id` int(11) NOT NULL,
+  `id` int(255) NOT NULL,
   `id_permission` int(11) NOT NULL,
   `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -90,7 +113,7 @@ INSERT INTO `permissions_users` (`id`, `id_permission`, `id_user`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(255) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -114,6 +137,19 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`) VALUES
 ALTER TABLE `articles`
   ADD PRIMARY KEY (`id`),
   ADD KEY `Saving author id for article` (`author_id`);
+
+--
+-- Index pour la table `articles_categories`
+--
+ALTER TABLE `articles_categories`
+  ADD KEY `id_article` (`id_article`),
+  ADD KEY `id_category` (`id_category`);
+
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `permissions`
@@ -143,25 +179,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `permissions`
 --
 ALTER TABLE `permissions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT pour la table `permissions_users`
 --
 ALTER TABLE `permissions_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Contraintes pour les tables déchargées
@@ -172,6 +214,13 @@ ALTER TABLE `users`
 --
 ALTER TABLE `articles`
   ADD CONSTRAINT `Saving author id for article` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `articles_categories`
+--
+ALTER TABLE `articles_categories`
+  ADD CONSTRAINT `id_article` FOREIGN KEY (`id_article`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_category` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `permissions_users`
