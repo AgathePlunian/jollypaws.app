e677d75715dd92b1893040167199f81b1583ddb2
BastienL <bastien.labouche96@orange.fr>
2021-10-07 11:25:26 +0200

    Mise en place de la corbeille

---
+++ views/admin/index/fr/sections/trash.php
@@ -0,0 +1,34 @@
+<?php
+	ob_start();
+?>
+
+<!-- Main div -->
+<div class="view" hidden="true" id="trash_article_view">
+	<h2 class="title-articles-list">Articles prochainement supprimés</h2>
+		<div class="article-en-redaction">
+			<?php
+				foreach($trashed_articles as $article){
+					?>
+					<div class="card-article">	
+						<div class="card-article-text">	
+							<p><span class="titles-card">Titre :</span> <?= $article['title'] ?> </p>
+							<p><span class="titles-card">Auteur :</span> <?= $article['firstname'] ?> <?= $article['lastname'] ?> </p>
+							<p><span class="titles-card">Date de création :</span> <?= $article['creation_date'] ?> </p>
+							<p><span class="titles-card">Dernière modification :</span> <?= $article['last_change_date'] ?> </p>
+						</div>
+						<div>
+							<a class="btn-empty-secondary" href="/<?= $lang ?>/article/show/<?= $article['id'] ?>">Prévisualiser</a>
+							<a class="btn-full-secondary" href="/<?= $lang ?>/admin/recover/<?= $article['id'] ?>">Récupérer</a>
+						</div>
+					</div>
+
+					<?php
+				}
+			?>
+		</div>
+</div>
+
+
+<?php
+	$trash = ob_get_clean();
+?>
