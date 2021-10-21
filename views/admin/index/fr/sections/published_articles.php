3ab2ee538cd56400804bf7dadc9206fc5f923642
AgathePlunian-ResilEyes <agathe.plunian@resileyes.com>
2021-10-21 14:36:55 +0200

    à la une

--- views/admin/index/fr/sections/published_articles.php
+++ views/admin/index/fr/sections/published_articles.php
@@ -5,6 +5,53 @@
 <!-- Main div -->
 <div class="view" hidden="true" id="published_articles_view">
 	<h1 class="title-articles-list">Articles publiés</h1>
+
+	<div class="a-la-une-header">
+		<h2 class="a-la-une-title">À la une</h2>
+		<a class="btn-full-primary">Modifier</a>
+	</div>
+	
+	<div class="a-la-une-container">
+		<div class="article-1-container">
+			<div class="a-la-une-container-img-1">
+				<img src="../../media/1c37aee7a97c3d80.jpeg"/>
+			</div>	
+			<div class="card-article-text">	
+				<p><span class="titles-card">Titre :</span></p>
+				<p><span class="titles-card">Auteur :</span></p>
+				<p><span class="titles-card">Date de création :</span></p>
+				<p><span class="titles-card">Dernière modification :</span></p>
+			</div>		
+		</div>
+		<div class="article-2-3-container">
+			<div class="article-2">
+				<div class="a-la-une-container-img-2-3">
+					<img src="../../media/1c37aee7a97c3d80.jpeg"/>
+				</div>	
+				<div class="card-article-text">	
+					<p><span class="titles-card">Titre :</span></p>
+					<p><span class="titles-card">Auteur :</span></p>
+					<p><span class="titles-card">Date de création :</span></p>
+					<p><span class="titles-card">Dernière modification :</span></p>
+				</div>		
+			</div>
+			<div class="article-3">
+				<div class="a-la-une-container-img-2-3">
+				<img src="../../media/1c37aee7a97c3d80.jpeg"/>
+				</div>	
+				<div class="card-article-text">	
+					<p><span class="titles-card">Titre :</span></p>
+					<p><span class="titles-card">Auteur :</span></p>
+					<p><span class="titles-card">Date de création :</span></p>
+					<p><span class="titles-card">Dernière modification :</span></p>
+				</div>		
+			</div>
+			
+		</div>
+	</div>
+
+	<h2 class="all-published-articles">Tous les articles publiés</h2>
+
 		<div class="article-en-redaction">
 			<?php
 				foreach($published_articles as $article){
