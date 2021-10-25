5a6c3f02c390b96c85db46429bf8d3c3ac4f1918
BastienL <bastien.labouche96@orange.fr>
2021-10-25 10:32:23 +0200

    Mise à jour bdd

--- user_database.sql
+++ user_database.sql
@@ -2,10 +2,10 @@
 -- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:3306
--- Généré le : jeu. 07 oct. 2021 à 21:41
--- Version du serveur :  10.3.31-MariaDB-0ubuntu0.20.04.1
--- Version de PHP : 7.4.3
+-- Host: localhost:3306
+-- Generation Time: Oct 25, 2021 at 10:31 AM
+-- Server version: 10.3.31-MariaDB-0ubuntu0.20.04.1
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `user_database`
+-- Database: `user_database`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `approbations`
+-- Table structure for table `approbations`
 --
 
 CREATE TABLE `approbations` (
@@ -36,7 +36,7 @@ CREATE TABLE `approbations` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `articles`
+-- Table structure for table `articles`
 --
 
 CREATE TABLE `articles` (
@@ -50,10 +50,24 @@ CREATE TABLE `articles` (
   `publish_date` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `articles`
+--
+
+INSERT INTO `articles` (`id`, `author_id`, `title`, `content`, `main_image`, `creation_date`, `last_change_date`, `publish_date`) VALUES
+(4, 1, 'azerzer', '[h2] Résumé [/h2]\r\n<a href=\"https://www.google.fr\"> Google </a>\r\n\r\n\r\n[h2] A retenir [/h2]\r\n\r\n[h2] En savoir plus [/h2]												', 'media/25f7de5436f14207.png', '2021-10-08 07:07:52', '2021-10-14 10:15:26', '2021-10-22 16:59:47'),
+(5, 2, 'azerzaerzar', '[h2] Résumé [/h2]\r\n\r\n[h2] A retenir [/h2]\r\n\r\n[h2] En savoir plus [/h2]				', 'media/8169243f87aaeb32.png', '2021-10-08 13:19:02', '2021-10-08 13:19:02', NULL),
+(6, 1, 'Ce titre ne fait aucun sens', '[h2] Résumé [/h2]\r\n\r\n[h2] A retenir [/h2]\r\n\r\n[B]Important :[/B]\r\n\r\n[UL]\r\n   [LI] toto [/LI]\\\r\n   [LI] tata [/LI]\\\r\n[/UL]\r\n\r\n\r\n[h2] En savoir plus [/h2]												', 'media/c807391a8b0de5d1.png', '2021-10-08 14:26:39', '2021-10-21 07:50:50', '2021-10-22 16:59:48'),
+(7, 1, 'Ceci est un article qui avant s\'appelait \"toto\"', '[h2] Résumé [/h2]\r\n\r\n[h2] A retenir [/h2]\r\n\r\n[h2] En savoir plus [/h2]								', 'media/14c2b568b7acc689.png', '2021-10-10 16:49:10', '2021-10-21 07:48:56', '2021-10-22 16:59:50'),
+(8, 1, 'toto', '[h2] Résumé [/h2]\r\n\r\n[h2] A retenir [/h2]\r\n\r\n[h2] En savoir plus [/h2]												', 'media/3b607c7f1c086648.png', '2021-10-10 16:50:23', '2021-10-13 14:03:45', '2021-10-22 16:59:50'),
+(9, 1, 'C\'est vraiment l\'article du désespoir', '[h2] Résumé [/h2]\r\n\r\n[h2] A retenir [/h2]\r\n\r\n[h2] En savoir plus [/h2]								', 'media/09c40f01afd7a232.png', '2021-10-10 17:00:14', '2021-10-10 17:00:14', '2021-10-22 16:59:51'),
+(10, 1, 'azertazgzertgretzefdgsdfazer', '[h2] Résumé [/h2]\r\n\r\n[h2] A retenir [/h2]\r\n\r\n[h2] En savoir plus [/h2]				', 'media/22a571a81df7cf30.png', '2021-10-22 08:16:33', '2021-10-22 08:16:33', '2021-10-22 16:59:52'),
+(11, 1, 'audrey', '[h2] Résumé [/h2]\r\n\r\n\r\n[B] toto [/B]\r\ntata\r\n\r\n\r\n[h2] A retenir [/h2]\r\nJ\'ai un énorme cerveau\r\n\r\n\r\n\r\n\r\n[h2] En savoir plus [/h2]																', 'media/42d1f66536e809f1.png', '2021-10-22 19:27:27', '2021-10-22 19:39:04', '2021-10-22 19:40:26');
+
 -- --------------------------------------------------------
 
 --
--- Structure de la table `articles_categories`
+-- Table structure for table `articles_categories`
 --
 
 CREATE TABLE `articles_categories` (
@@ -61,21 +75,43 @@ CREATE TABLE `articles_categories` (
   `id_category` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `articles_categories`
+--
+
+INSERT INTO `articles_categories` (`id_article`, `id_category`) VALUES
+(6, 1),
+(6, 2),
+(6, 3),
+(9, 1),
+(9, 2),
+(11, 1),
+(11, 3);
+
 -- --------------------------------------------------------
 
 --
--- Structure de la table `articles_published`
+-- Table structure for table `articles_published`
 --
 
 CREATE TABLE `articles_published` (
-  `id` int(255) NOT NULL,
   `id_article` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `articles_published`
+--
+
+INSERT INTO `articles_published` (`id_article`) VALUES
+(7),
+(8),
+(9),
+(10);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `categories`
+-- Table structure for table `categories`
 --
 
 CREATE TABLE `categories` (
@@ -84,19 +120,42 @@ CREATE TABLE `categories` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `categories`
+-- Dumping data for table `categories`
 --
 
 INSERT INTO `categories` (`id`, `name`) VALUES
 (1, 'PTSD'),
 (2, 'Enfant'),
 (3, 'TRAUMA'),
-(4, 'POMPIERS');
+(4, 'POMPIERS'),
+(5, 'UNIFORME'),
+(8, 'hvhukvvkjvk');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `permissions`
+-- Table structure for table `front_page_articles`
+--
+
+CREATE TABLE `front_page_articles` (
+  `id` int(255) NOT NULL,
+  `id_article` int(255) DEFAULT NULL,
+  `importance` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `front_page_articles`
+--
+
+INSERT INTO `front_page_articles` (`id`, `id_article`, `importance`) VALUES
+(1, 8, 1),
+(2, NULL, 2),
+(3, 9, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
 --
 
 CREATE TABLE `permissions` (
@@ -105,7 +164,7 @@ CREATE TABLE `permissions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `permissions`
+-- Dumping data for table `permissions`
 --
 
 INSERT INTO `permissions` (`id`, `name`) VALUES
@@ -113,21 +172,22 @@ INSERT INTO `permissions` (`id`, `name`) VALUES
 (2, 'DEL_ARTICLE'),
 (3, 'APPROVE_ARTICLE'),
 (4, 'PUBLISH_ARTICLE'),
-(5, 'CREATE_ACCOUNT');
+(5, 'CREATE_ACCOUNT'),
+(6, 'MANAGE_CATEGORIES');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `permissions_users`
+-- Table structure for table `permissions_users`
 --
 
 CREATE TABLE `permissions_users` (
   `id_permission` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL
+  `id_user` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `permissions_users`
+-- Dumping data for table `permissions_users`
 --
 
 INSERT INTO `permissions_users` (`id_permission`, `id_user`) VALUES
@@ -183,12 +243,13 @@ INSERT INTO `permissions_users` (`id_permission`, `id_user`) VALUES
 (2, 11),
 (3, 11),
 (4, 11),
-(5, 11);
+(5, 11),
+(6, 1);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `trash`
+-- Table structure for table `trash`
 --
 
 CREATE TABLE `trash` (
@@ -199,7 +260,7 @@ CREATE TABLE `trash` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -211,7 +272,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`) VALUES
@@ -230,7 +291,7 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `waiting_approval`
+-- Table structure for table `waiting_approval`
 --
 
 CREATE TABLE `waiting_approval` (
@@ -239,160 +300,172 @@ CREATE TABLE `waiting_approval` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Index pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `approbations`
+-- Indexes for table `approbations`
 --
 ALTER TABLE `approbations`
   ADD KEY `id approbation request` (`id_approbation_request`),
   ADD KEY `id user` (`id_user`);
 
 --
--- Index pour la table `articles`
+-- Indexes for table `articles`
 --
 ALTER TABLE `articles`
   ADD PRIMARY KEY (`id`),
   ADD KEY `Saving author id for article` (`author_id`);
 
 --
--- Index pour la table `articles_categories`
+-- Indexes for table `articles_categories`
 --
 ALTER TABLE `articles_categories`
   ADD KEY `id_article` (`id_article`),
   ADD KEY `id_category` (`id_category`);
 
 --
--- Index pour la table `articles_published`
+-- Indexes for table `articles_published`
 --
 ALTER TABLE `articles_published`
-  ADD PRIMARY KEY (`id`),
   ADD KEY `id article` (`id_article`);
 
 --
--- Index pour la table `categories`
+-- Indexes for table `categories`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `permissions`
+-- Indexes for table `front_page_articles`
+--
+ALTER TABLE `front_page_articles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id article front page article` (`id_article`);
+
+--
+-- Indexes for table `permissions`
 --
 ALTER TABLE `permissions`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `permissions_users`
+-- Indexes for table `permissions_users`
 --
 ALTER TABLE `permissions_users`
   ADD KEY `permissions` (`id_permission`) USING BTREE,
   ADD KEY `users` (`id_user`);
 
 --
--- Index pour la table `trash`
+-- Indexes for table `trash`
 --
 ALTER TABLE `trash`
   ADD KEY `trashed_article` (`id_article`);
 
 --
--- Index pour la table `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `waiting_approval`
+-- Indexes for table `waiting_approval`
 --
 ALTER TABLE `waiting_approval`
   ADD PRIMARY KEY (`id`),
   ADD KEY `article waiting approval` (`id_article`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `articles`
+-- AUTO_INCREMENT for table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT pour la table `articles_published`
---
-ALTER TABLE `articles_published`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT pour la table `categories`
+-- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT pour la table `permissions`
+-- AUTO_INCREMENT for table `front_page_articles`
+--
+ALTER TABLE `front_page_articles`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `permissions`
 --
 ALTER TABLE `permissions`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT pour la table `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT pour la table `waiting_approval`
+-- AUTO_INCREMENT for table `waiting_approval`
 --
 ALTER TABLE `waiting_approval`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
--- Contraintes pour les tables déchargées
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `approbations`
+-- Constraints for table `approbations`
 --
 ALTER TABLE `approbations`
   ADD CONSTRAINT `id approbation request` FOREIGN KEY (`id_approbation_request`) REFERENCES `waiting_approval` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `id user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `articles`
+-- Constraints for table `articles`
 --
 ALTER TABLE `articles`
   ADD CONSTRAINT `Saving author id for article` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `articles_categories`
+-- Constraints for table `articles_categories`
 --
 ALTER TABLE `articles_categories`
   ADD CONSTRAINT `id_article` FOREIGN KEY (`id_article`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `id_category` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `articles_published`
+-- Constraints for table `articles_published`
 --
 ALTER TABLE `articles_published`
   ADD CONSTRAINT `id article` FOREIGN KEY (`id_article`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `permissions_users`
+-- Constraints for table `front_page_articles`
+--
+ALTER TABLE `front_page_articles`
+  ADD CONSTRAINT `id article front page article` FOREIGN KEY (`id_article`) REFERENCES `articles_published` (`id_article`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `permissions_users`
 --
 ALTER TABLE `permissions_users`
   ADD CONSTRAINT `permissions` FOREIGN KEY (`id_permission`) REFERENCES `permissions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `trash`
+-- Constraints for table `trash`
 --
 ALTER TABLE `trash`
   ADD CONSTRAINT `trashed_article` FOREIGN KEY (`id_article`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `waiting_approval`
+-- Constraints for table `waiting_approval`
 --
 ALTER TABLE `waiting_approval`
   ADD CONSTRAINT `article waiting approval` FOREIGN KEY (`id_article`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
